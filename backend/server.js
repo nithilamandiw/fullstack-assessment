004e1fa062d721df505c885ed7ceabfc3abcc23e
Nithila Mandiw <nithilamandiw@gmail.com>
2026-05-17 13:27:25 +0530

    Update globals.css

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://fullstack-assessment-orcin.vercel.app",
+  ],
+}));
 app.use(express.json());
 
 // Routes
